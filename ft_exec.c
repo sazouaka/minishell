3819e5c9d778f115da6245c19f266c1d44e426d8
Samira Azouakan <sazouaka@e1r8p3.1337.ma>
2019-11-09 18:43:28 +0100

    ft

---
+++ ft_exec.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_access.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sazouaka <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 16:32:24 by sazouaka          #+#    #+#             */
+/*   Updated: 2019/11/07 16:32:30 by sazouaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_access(char **paths, char **flag)
+{
+	int		i;
+	char	*cmd_path;
+
+	i = 0;
+	while (paths[i])
+	{
+		if (access(ft_strjoin(paths[i], flag[0]), X_OK) == 0)
+		{
+			cmd_path = ft_strjoin(paths[i], flag[0]);
+			return (cmd_path);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	ft_exec(char **paths, char **flag)
+{
+	char	*cmd_path;
+	pid_t	pid;
+
+	cmd_path = ft_access(paths, flag);
+	if (cmd_path)
+	{
+		pid = fork();
+		if (fork < 0)
+		{
+			ft_putstr("fork failed\n");
+			exit(1);
+		}
+		else if (pid == 0)
+			execv(cmd_path, flag);
+		wait(NULL);
+	}
+	// else if (cmd_path == NULL)
+	// 	ft_putstr("zsh: command not found: ");
+	// 	ft_putstr(flag[0]);
+	// 	ft_putchar('\n');
+}
